f23b157ebc314a642eb119aad51361a1e2ff2a4c
kyleeap <kyleeap@csl4wk67.win2k.cs.ust.hk>
2019-11-07 19:11:40 +0800

    wadasfafsa

--- Yankees/src/main/java/Yankees/Library.java
+++ Yankees/src/main/java/Yankees/Library.java
@@ -6,7 +6,7 @@ package Yankees;
 public class Library {
 	
 	public static void main(String arg[]) {
-		System.out.println("HIHIHIHIHIHHII");
+		System.out.println("new HIHIHIHIHIHHII");
 	}
     public boolean someLibraryMethod() {
         return true;
